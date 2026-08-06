180685355c6afb38b84e92f2ae1d8490b2a585d3
mykola-lp <mykola.sukhorab@gmail.com>
2026-08-06 18:34:06 +0300

    add AppBar component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import { View, StyleSheet } from 'react-native';
 
 import RepositoryList from './RepositoryList';
+import AppBar from './AppBar';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,6 +14,7 @@ const styles = StyleSheet.create({
 const Main = () => {
   return (
     <View style={styles.container}>
+      <AppBar />
       <RepositoryList />
     </View>
   );
