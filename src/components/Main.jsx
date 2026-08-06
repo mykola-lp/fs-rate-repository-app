beae5529aec8fb84a68e4734fa58348cee714f49
mykola-lp <mykola.sukhorab@gmail.com>
2026-08-06 15:20:39 +0300

    upd Main.jsx file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,19 @@
-import Constants from 'expo-constants';
-import { Text, StyleSheet, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+
+import RepositoryList from './RepositoryList';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
-    flex: 1,
+    flexGrow: 1,
+    flexShrink: 1,
+    backgroundColor: '#e1e4e8',
   },
 });
 
 const Main = () => {
   return (
     <View style={styles.container}>
-      <Text>Rate Repository Application</Text>
+      <RepositoryList />
     </View>
   );
 };
