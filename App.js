3a4dcf453fc716217ad039ed6b001cd46d55fb42
mykola-lp <mykola.sukhorab@gmail.com>
2026-08-10 16:07:16 +0300

    initialize the storage in the App component

--- App.js
+++ App.js
@@ -6,8 +6,10 @@ import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
 
 import createApolloClient from './src/utils/apolloClient';
+import AuthStorage from './src/utils/authStorage';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
