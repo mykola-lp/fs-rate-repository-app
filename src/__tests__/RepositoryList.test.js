61fc103f730114f4d85764dd0e21e80ea1e95eb3
mykola-lp <mykola.sukhorab@gmail.com>
2026-08-11 13:19:05 +0300

    wrap RepositoryListContainer test in NativeRouter

--- src/__tests__/RepositoryList.test.js
+++ src/__tests__/RepositoryList.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react-native';
+import { NativeRouter } from 'react-router-native';
 
 import { RepositoryListContainer } from '../components/RepositoryList';
 
@@ -45,7 +46,11 @@ describe('RepositoryList', () => {
         ],
       };
 
-      await render(<RepositoryListContainer repositories={repositories} />);
+      await render(
+        <NativeRouter>
+          <RepositoryListContainer repositories={repositories} />
+        </NativeRouter>,
+      );
 
       const repositoryItems = screen.getAllByTestId('repositoryItem');
       const [firstRepositoryItem, secondRepositoryItem] = repositoryItems;
